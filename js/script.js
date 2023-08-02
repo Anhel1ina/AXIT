9915a968925c1d97163414de2cae5e0dc67145a4
Anhelina Daineka <dnk.angelina@gmail.com>
2023-08-02 22:30:15 +0300

    Modified swiper

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 let prevScrollpos = window.scrollY;
 window.onscroll = function () {
-    var currentScrollPos = window.scrollY;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("scroll-menu").style.top = "0";
-    } else {
-        document.getElementById("scroll-menu").style.top = "-76px";
+    let currentScrollPos = window.scrollY;
+    if(window.innerWidth > 720){
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("scroll-menu").style.top = "0";
+        } else {
+            document.getElementById("scroll-menu").style.top = "-76px";
+        }
+        prevScrollpos = currentScrollPos;
     }
-    prevScrollpos = currentScrollPos;
+    else{
+        document.getElementById("scroll-menu").style.top = "0";
+    }
 }
